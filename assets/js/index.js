2574f056e16af3f1aa2d71abb2b31b8ea8c2d78b
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-03-02 21:03:20 -0300

    se crearon los filtros para crear las cards segun los inputs, se importa la data desde un js, se creo la card not found

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,214 +1,56 @@
-const data = {
-  "currentDate": "2022-01-01",
-  "events": [
-    {
-      _id: 1,
-      "image":"https://i.postimg.cc/Fs03hQDt/Collectivities-Party.jpg",
-      "name":"Collectivities Party",
-      "date":"2021-12-12",
-      "description":"Enjoy your favourite dishes, from different countries, in a unique event for the whole family.",
-      "category":"Food Fair",
-      "place":"Room A",
-      "capacity":45000,
-      "assistance":42756,
-      "price":5
-    },
-    {
-      _id: 2,
-      "image":"https://i.postimg.cc/ZmD3Xf57/Korean-style.jpg",
-      "name":"Korean style",
-      "date":"2022-08-12",
-      "description":"Enjoy the best Korean dishes, with international chefs and awesome events.",
-      "category":"Food Fair",
-      "place":"Room A",
-      "capacity":45000,
-      "assistance":42756,
-      "price":10
-    },
-    {
-      _id: 3,
-      "image":"https://i.postimg.cc/GmHRkbNV/Jurassic-Park.jpg",
-      "name":"Jurassic Park",
-      "date":"2021-11-02",
-      "description":"Let's go meet the biggest dinosaurs in the paleontology museum.",
-      "category":"Museum",
-      "place":"Field",
-      "capacity":82000,
-      "assistance":65892,
-      "price":15
-    },
-    {
-      _id: 4,
-      "image":"https://i.postimg.cc/c4C2zXm8/Parisian-Museum.jpg",
-      "name":"Parisian Museum",
-      "date":"2022-11-02",
-      "description":"A unique tour in the city of lights, get to know one of the most iconic places.",
-      "category":"Museum",
-      "place":"Paris",
-      "capacity":8200,
-      "estimate":8200,
-      "price":3500
-    },
-    {
-      _id: 5,
-      "image":"https://i.postimg.cc/KYD0jMf2/comicon.jpg",
-      "name":"Comicon",
-      "date":"2021-02-12",
-      "description":"For comic lovers, all your favourite characters gathered in one place.",
-      "category":"Costume Party",
-      "place":"Room C",
-      "capacity":120000,
-      "assistance":110000,
-      "price":54
-    },
-    {
-      _id: 6,
-      "image":"https://i.postimg.cc/RZ9fH4Pr/halloween.jpg",
-      "name":"Halloween Night",
-      "date":"2022-02-12",
-      "description":"Come with your scariest costume and win incredible prizes.",
-      "category":"Costume Party",
-      "place":"Room C",
-      "capacity":12000,
-      "estimate":9000,
-      "price":12
-    },
-    {
-      _id: 7,
-      "image":"https://i.postimg.cc/PrMJ0ZMc/Metallica-in-concert.jpg",
-      "name":"Metallica in concert",
-      "date":"2022-01-22",
-      "description":"The only concert of the most emblematic band in the world.",
-      "category":"Music Concert",
-      "place":"Room A"
-      ,"capacity":138000,
-      "estimate":138000,
-      "price":150
-    },
-    {
-      _id: 8,
-      "image":"https://i.postimg.cc/KvsSK8cj/Electronic-Fest.jpg",
-      "name":"Electronic Fest",
-      "date":"2021-01-22",
-      "description":"The best national and international DJs gathered in one place.",
-      "category":"Music Concert",
-      "place":"Room A",
-      "capacity":138000,
-      "assistance":110300,
-      "price":250
-      },
-    {
-      _id: 9,
-      "image":"https://i.postimg.cc/fyLqZY9K/10-K-for-life.jpg",
-      "name":"10K for life",
-      "date":"2021-03-01",
-      "description":"Come and exercise, improve your health and lifestyle.",
-      "category":"Race",
-      "place":"Soccer field",
-      "capacity":30000,
-      "assistance":25698,
-      "price":3
-    },
-    {
-      _id: 10,
-      "image":"https://i.postimg.cc/zv67r65z/15kny.jpg",
-      "name":"15K NY",
-      "date":"2022-03-01",
-      "description":"We'll be raising funds for hospitals and medical care in this unique event held in The Big Apple.",
-      "category":"Race",
-      "place":"New York",
-      "capacity":3000000,
-      "assistance":2569800,
-      "price":3
-      },
-    {
-      _id: 11,
-      "image":"https://i.postimg.cc/Sst763n6/book1.jpg",
-      "name":"School's book fair",
-      "date":"2022-10-15",
-      "description":"Bring your unused school book and take the one you need.",
-      "category":"Book Exchange",
-      "place":"Room D1",
-      "capacity":150000,
-      "estimate":123286,
-      "price":1
-    },
-    {
-      _id: 12,
-      "image":"https://i.postimg.cc/05FhxHVK/book4.jpg",
-      "name":"Just for your kitchen",
-      "date":"2021-11-09",
-      "description":"If you're a gastronomy lover come get the cookbook that best suits your taste and your family's.",
-      "category":"Book Exchange",
-      "place":"Room D6",
-      "capacity":130000,
-      "assistance":90000,
-      "price":100
-    },
-    {
-      _id: 13,
-      "image":"https://i.postimg.cc/vH52y81C/cinema4.jpg",
-      "name":"Batman",
-      "date":"2021-3-11",
-      "description":"Come see Batman fight crime in Gotham City.",
-      "category":"Cinema",
-      "place":"Room D1",
-      "capacity":11000,
-      "assistance":9300,
-      "price":225
-    },
-    {
-      _id: 14,
-      "image":"https://i.postimg.cc/T3C92KTN/scale.jpg",
-      "name":"Avengers",
-      "date":"2022-10-15",
-      "description":"Marvel's Avengers Premier in 3d, the start of an epic saga with your favourite superheroes.",
-      "category":"Cinema",
-      "place":"Room D1",
-      "capacity":9000,
-      "estimate":9000,
-      "price":250
-    }
-  ]
-}
+import data from './data.js';
 
 //cuando se capturan elementos HTML se usa signo $
 
+//Funcion para crear las cards
 
 function createCards(array, idTemplate, idDiv){
   
   const $cards = document.getElementById(idDiv);
   const $template = document.getElementById(idTemplate).content;
   const fragment = document.createDocumentFragment();
-
-  for (let i = 0; i < array.events.length; i++) {
-    $template.querySelector('section span').style.backgroundImage = `url(${array.events[i].image})`
-    $template.querySelector('section h3').textContent = array.events[i].name
-    $template.querySelector('section h5').textContent = array.events[i].description
-    $template.querySelector('section div p').textContent = `Price $${array.events[i].price},00`
-    const clone = $template.cloneNode(true)
-    fragment.appendChild(clone)
+  $cards.textContent = "";
+  
+  if (!array.length == 0){
+    for (let i = 0; i < array.length; i++) {
+      $template.querySelector('section').setAttribute('id', array[i].category)
+      $template.querySelector('section span').style.backgroundImage = `url(${array[i].image})`
+      $template.querySelector('section h3').textContent = array[i].name
+      $template.querySelector('section h5').textContent = array[i].description
+      $template.querySelector('section div p').textContent = `Price $${array[i].price},00`
+      $template.querySelector('section div a').style.display = "flex"
+      let clone = $template.cloneNode(true)
+      fragment.appendChild(clone)
+    }
+  } else {
+    let $section = document.createElement('section')
+    $section.innerHTML = `<span class="img" style='background-image: url("./assets/NotFound.gif");'></span>
+                          <h3>Not Result Found 😿</h3>
+                          <h5>Sorry, we couldn't find any results</h5>`
+    fragment.appendChild($section)
   }
 
   $cards.appendChild(fragment)
 
-  document.getElementById(idTemplate).remove();
 }
-createCards(data, "template", "cards")
 
-//Funcion para los filtros de los Checkbox de Categorias de forma dinámica
+//Llamamos la funcion cards cuando inicia la pagina
+createCards(data.events, "template", "cards")
 
-let arrayCategories = data.events.map(objeto => objeto = objeto.category)
+//Crear los Checkbox de Categorias de forma dinámica
 
-let arrayCategoriesWithOutDuplicated = arrayCategories.filter((item,index)=>{
-    return arrayCategories.indexOf(item) === index;
-  })
+let arrayCategories = data.events.map(e => e.category).reduce((acc, category)=>
+{if (!acc.includes(category)){
+  acc.push(category)
+}
+return acc;
+},[]
+)
 
 const $filters = document.getElementById('filters');
-
 const fragment2 = document.createDocumentFragment();
 
-for (let category of arrayCategoriesWithOutDuplicated) {
+for (let category of arrayCategories) {
     let $label = document.createElement("label");
     $label.textContent = category
     $label.setAttribute("for",category)
@@ -221,3 +63,59 @@ for (let category of arrayCategoriesWithOutDuplicated) {
 }
 
 $filters.appendChild(fragment2)
+
+//Filtrar por categoría
+let filteredData = data.events
+
+function filterByCategory(array, cat){
+  let dataFiltered = array.filter(e => cat.toLowerCase().includes(e.category.toLowerCase()))
+  return dataFiltered
+}
+
+$filters.addEventListener("change", function(){
+  let $checkbox = $filters.getElementsByTagName("input")
+  let arra = ""
+  // $search.value = ""
+
+  for (let i = 0; i < $checkbox.length; i++) {
+    if($checkbox[i].checked){
+      arra += $checkbox[i].id.toLocaleLowerCase()
+    }
+  }
+  if (!arra == ""){
+    filteredData = filterByCategory(data.events,arra)
+  }
+  else{
+    filteredData = data.events
+  }
+  createCards(filteredData, "template", "cards")
+})
+
+//Filtrar por palabra Clave
+
+function filterByWord(array, keyWord){
+  let dataFiltered = array.filter(e => e.name.toLowerCase().includes(keyWord.toLowerCase()))
+  return dataFiltered
+}
+
+const $search = document.getElementById("search");
+
+let filteredData2 = filteredData
+
+$search.addEventListener("keyup", function(e){
+  if (e.key === "Escape")
+  {$search.value = ""
+  filteredData2 = filteredData
+  }
+  filteredData2 = filterByWord(filteredData, $search.value)
+  createCards(filteredData2, "template", "cards")
+})
+
+//Crear Cards Filtradas en Submit
+
+const $formSearch = document.getElementById("formSearch");
+
+$formSearch.addEventListener("submit", function(e){
+  e.preventDefault()
+  createCards(filteredData2, "template", "cards")
+})
